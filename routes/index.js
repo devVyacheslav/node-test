b8672d412fc84425ca8e8c9c719f107e2222d7ae
andrIvash <andr.ivas12@gmail.com>
2016-06-03 22:11:41 +0300

    add data to mongodb

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://188.225.36.182/test');
+
+var Cat = mongoose.model('Cat', { name: String });
+
+var kitty = new Cat({ 
+  name: 'Zildjian',
+  say: 'Hello World' 
+});
+
+kitty.save(function (err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('meow');
+  }
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  Cat.find(function (err, kittys) {
+    res.render('index', { 
+      title: 'Express',
+      cats: kittys 
+
+    });
+  });
+  
 });
 
 module.exports = router;
