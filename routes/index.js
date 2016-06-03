d0eb72c31f473ac003b80cf11381c9e08c1dbc91
andrIvash <andr.ivas12@gmail.com>
2016-06-03 23:04:20 +0300

    next attempt

--- routes/index.js
+++ routes/index.js
@@ -3,30 +3,11 @@ var router = express.Router();
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/test');
 
-var Cat = mongoose.model('Cat', { name: String });
 
-var kitty = new Cat({ 
-  name: 'Zildjian',
-  say: 'Hello World'
-
-});
-kitty.save(function (err) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log('meow');
-  }
-});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    Cat.find(function (err, kittys) {
-      res.render('index', { 
-        title: 'Express',
-        cats: kittys
-      });
-    });
-    
+    res.render('index', { title: 'Express'});
 });
 
 module.exports = router;
