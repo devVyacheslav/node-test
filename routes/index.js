2153ee2954bcc737ee415df4cca081752e716921
andrIvash <andr.ivas12@gmail.com>
2016-06-03 22:43:50 +0300

    add mongo and data

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,15 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://188.225.36.182/test');
+mongoose.connect('mongodb://localhost/test');
 
 var Cat = mongoose.model('Cat', { name: String });
 
 var kitty = new Cat({ 
   name: 'Zildjian',
-  say: 'Hello World' 
-});
+  say: 'Hello World'
 
+});
 kitty.save(function (err) {
   if (err) {
     console.log(err);
@@ -20,14 +20,13 @@ kitty.save(function (err) {
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  Cat.find(function (err, kittys) {
-    res.render('index', { 
-      title: 'Express',
-      cats: kittys 
-
+    Cat.find(function (err, kittys) {
+      res.render('index', { 
+        title: 'Express',
+        cats: kittys
+      });
     });
-  });
-  
+    
 });
 
 module.exports = router;
