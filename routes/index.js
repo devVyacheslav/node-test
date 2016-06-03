bdcec80a225206af8de0516a2e5d85eb201dc5fb
andrIvash <andr.ivas12@gmail.com>
2016-06-03 23:11:11 +0300

    add data

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,23 @@ var router = express.Router();
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/test');
 
+var Cat = mongoose.model('Cat', { name: String });
+
+var kitty = new Cat({ name: 'Zildjian' });
+
+kitty.save();
+
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express'});
+    Cat.find(function (err, cats) {
+      res.render('index', { 
+        title: 'Express',
+        cats: cats
+      });
+    });
+    
 });
 
 module.exports = router;
